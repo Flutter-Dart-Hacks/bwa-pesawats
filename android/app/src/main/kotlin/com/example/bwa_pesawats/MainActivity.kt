c5f40a317cdd05fa3423c93954c11fbca5d747dd
GulajavaMinistudio <gulajava.mini@gmail.com>
2023-01-24 16:47:10 +0700

    Inisialisasi aplikasi pertama kali

---
+++ android/app/src/main/kotlin/com/example/bwa_pesawats/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bwa_pesawats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/shareds/themes.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xff5C40CC);
+const kBlackColor = Color(0xff1F1449);
+const kWhiteColor = Color(0xffFFFFFF);
+const kGreyColor = Color(0xff9698A9);
+const kGreenColor = Color(0xff0EC3AE);
+const kRedColor = Color(0xffEB70A5);
