b0b4a4becc425a42ddcdc14936a9c0637640964e
GulajavaMinistudio <gulajava.mini@gmail.com>
2023-01-25 11:14:16 +0700

    Persiapan menjadi themes dan package

--- lib/shareds/themes.dart
+++ lib/shareds/themes.dart
@@ -1,8 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-const kPrimaryColor = Color(0xff5C40CC);
-const kBlackColor = Color(0xff1F1449);
-const kWhiteColor = Color(0xffFFFFFF);
-const kGreyColor = Color(0xff9698A9);
-const kGreenColor = Color(0xff0EC3AE);
-const kRedColor = Color(0xffEB70A5);
+const Color kPrimaryColor = Color(0xff5C40CC);
+const Color kBlackColor = Color(0xff1F1449);
+const Color kWhiteColor = Color(0xffFFFFFF);
+const Color kGreyColor = Color(0xff9698A9);
+const Color kGreenColor = Color(0xff0EC3AE);
+const Color kRedColor = Color(0xffEB70A5);
+const Color kBackgroundColor = Color(0xffFAFAFA);
+const Color kInactiveColor = Color(0xffDBD7EC);
+
+TextStyle blackTextStyle = GoogleFonts.poppins().copyWith(
+  color: kBlackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins().copyWith(
+  color: kWhiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins().copyWith(
+  color: kGreyColor,
+);
+
+TextStyle greenTextStyle = GoogleFonts.poppins().copyWith(
+  color: kGreenColor,
+);
+
+TextStyle redTextStyle = GoogleFonts.poppins().copyWith(
+  color: kRedColor,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins().copyWith(
+  color: kPrimaryColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight blackBold = FontWeight.w900;
