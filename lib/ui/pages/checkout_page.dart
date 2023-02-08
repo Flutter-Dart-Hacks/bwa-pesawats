f398a4f0d8cfbcdf5ab9b34e167df88e83037161
GulajavaMinistudio <gulajava.mini@gmail.com>
2023-02-08 14:47:41 +0700

    Penambahan detail checkout lengkap

--- lib/ui/pages/checkout_page.dart
+++ lib/ui/pages/checkout_page.dart
@@ -1,5 +1,6 @@
 import 'package:bwa_pesawats/shareds/themes.dart';
 import 'package:bwa_pesawats/ui/widgets/booking_detail_item.dart';
+import 'package:bwa_pesawats/ui/widgets/custom_button_getstarted.dart';
 import 'package:flutter/material.dart';
 
 class CheckoutPage extends StatefulWidget {
@@ -201,7 +202,160 @@ class _CheckoutPageState extends State<CheckoutPage> {
               valueText: 'NO',
               valueColor: kRedColor,
             ),
+            const BookingDetailItem(
+              title: 'VAT',
+              valueText: '45%',
+              valueColor: kBlackColor,
+            ),
+            const BookingDetailItem(
+              title: 'Price',
+              valueText: 'IDR 8.500.690',
+              valueColor: kBlackColor,
+            ),
+            const BookingDetailItem(
+              title: 'Grand Total',
+              valueText: 'IDR 12.000.000',
+              valueColor: kPrimaryColor,
+            ),
           ],
+        ),
+      );
+    }
+
+    Widget createPaymentDetails() {
+      return Container(
+        margin: const EdgeInsets.symmetric(vertical: 30),
+        padding: const EdgeInsets.only(
+          left: 20,
+          right: 20,
+          top: 30,
+          bottom: 30,
+        ),
+        decoration: const BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.all(
+            Radius.circular(18),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Payment Details',
+              style: blackTextStyle.copyWith(
+                fontWeight: semiBold,
+                fontSize: 16,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 16),
+              child: Row(
+                children: [
+                  // NOTE CARD IMAGE
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      width: 100,
+                      height: 70,
+                      margin: const EdgeInsets.only(right: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(18),
+                        image: const DecorationImage(
+                            image: AssetImage('resources/image_card.png'),
+                            fit: BoxFit.cover),
+                      ),
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 24,
+                              height: 24,
+                              margin: const EdgeInsets.only(right: 6),
+                              decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage('resources/icon_plane.png'),
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'Pay',
+                              style: whiteTextStyle.copyWith(
+                                fontWeight: medium,
+                                fontSize: 16,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  // NOTE PRICE TOTAL BALANCE
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'IDR 80.400.000',
+                          style: blackTextStyle.copyWith(
+                            fontWeight: semiBold,
+                            fontSize: 18,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          'Current Balance',
+                          style: blackTextStyle.copyWith(
+                            fontWeight: light,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget createPaynowButton() {
+      return Container(
+        width: double.infinity,
+        height: 55,
+        margin: const EdgeInsets.only(bottom: 30),
+        child: CustomButtonPrimary(
+          title: 'Pay Now',
+          onPressedFunction: () {},
+        ),
+      );
+    }
+
+    Widget termConditionWidget() {
+      return Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.only(
+          bottom: 73,
+        ),
+        child: TextButton(
+          onPressed: () {},
+          style: TextButton.styleFrom(
+            foregroundColor: kGreyColor,
+          ),
+          child: Text(
+            'Terms and Condition',
+            style: greyTextStyle.copyWith(
+              fontWeight: light,
+              fontSize: 16,
+              decoration: TextDecoration.underline,
+              decorationThickness: 2,
+            ),
+          ),
         ),
       );
     }
@@ -215,6 +369,9 @@ class _CheckoutPageState extends State<CheckoutPage> {
           children: [
             createRouteWidget(),
             createBookingDetails(),
+            createPaymentDetails(),
+            createPaynowButton(),
+            termConditionWidget(),
           ],
         ),
       ),
